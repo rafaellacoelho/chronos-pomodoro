53a44885e9dd30efcb1a2ca5961b4961ae9d7aef
Rafaella Coelho <rafaellacoelho@icloud.com>
2025-10-07 17:56:29 -0300

    movendo components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   console.log('Oi');
 
   return (
     <>
-      <Heading>
-        Olá Mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-        officiis possimus vel laboriosam aperiam placeat perspiciatis pariatur
-        quasi debitis nulla ducimus alias aliquid at, maxime quo, expedita qui
-        nihil. Expedita!
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
