3e7eca76c4be00b71dc1e1c8994ad1d1f4091634
Rafaella Coelho <rafaellacoelho@icloud.com>
2025-10-07 15:43:23 -0300

    Finalização do componente Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   console.log('Oi');
 
   return (
     <>
-      <Heading />
+      <Heading>
+        Olá Mundo 1
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
         officiis possimus vel laboriosam aperiam placeat perspiciatis pariatur
