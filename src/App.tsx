ef400f1e8887ad28127fbe0b8b3c90d8fe805ec3
Rafaella Coelho <rafaellacoelho@icloud.com>
2025-10-07 22:23:39 -0300

    estrutura básica do countdown

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './styles/global.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   console.log('Oi');
@@ -16,6 +17,10 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
