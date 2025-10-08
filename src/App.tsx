dcad538bb22e41d807138a043394661c7b918556
Rafaella Coelho <rafaellacoelho@icloud.com>
2025-10-07 22:14:04 -0300

    criando os itens de menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 export function App() {
   console.log('Oi');
@@ -15,7 +15,7 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>Logo</Heading>
+        <Menu />
       </Container>
     </>
   );
