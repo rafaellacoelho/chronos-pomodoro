db21e7c28e5434c92e9448c0901f8018993f16b9
Rafaella Coelho <rafaellacoelho@icloud.com>
2025-10-09 19:21:28 -0300

    criando o objeto de dark e light com useState e useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   console.log('Oi');
@@ -45,9 +48,13 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} color='red' />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
